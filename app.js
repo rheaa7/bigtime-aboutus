795a2edf59cf4419858bb8348da23ac2bba3e6a5
rheaa7 <rheaa@uw.edu>
2014-12-04 00:30:43 -0800

    version 1
    about us page

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$( document ).ready(function() {
+    
+    //creates the google map and info window on the page 
+    var mapOptions = {
+        center: {lat: 47.65785, lng: -122.31342}, 
+        zoom: 12 
+    };
+    
+    var mapElem = document.getElementById('map');
+    var map = new google.maps.Map(mapElem, mapOptions);
+    var infoWin = new google.maps.InfoWindow();
+
+    //resize the map based on the viewport height
+    $(window).resize(function() {
+        $("#map").height( (Number($(this).height())) - ($('#map').position().top) - 20); 
+
+    });
+    
+}); //DOM
